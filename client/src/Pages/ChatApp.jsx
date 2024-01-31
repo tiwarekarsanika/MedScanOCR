59fa60fff78c801a953fc04f50090f8c1ea53c58
YS028 <ythink28@gmail.com>
2024-01-31 23:59:05 +0530

    naya edit

--- client/src/Pages/ChatApp.jsx
+++ client/src/Pages/ChatApp.jsx
@@ -1,51 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import SendIcon from '@mui/icons-material/Send';
-import PatientCard from '../components/PatientCard';
+import SendIcon from '@mui/icons-material/Send'
+import PatientCard from '../components/PatientCard'
+import logo from '../assets/logo2.png'
+
+import Alert from '@mui/material/Alert';
 
 const Wrapper = styled.div`
-width:100%;
-height:100vh;
-background-color:black;
-display:flex;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  display: flex;
 `
 const Patients = styled.div`
-height:100;
-flex:3;
-background-color:white;
-display:flex; 
- flex-direction:column;
- align-items:center; 
+  height: 100;
+  flex: 3;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-.title{
-    width:100%;
-    font-size:1.5rem;
-    flex:1;
-    display:flex;
-    align-items:center;
-    font-weight:700;
-    font-size:2rem;
-    margin-left:10%;
-    color:var(--primary-color)
-
-}
+  .title {
+    width: 100%;
+    font-size: 1.5rem;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+    font-size: 2rem;
+    margin-left: 10%;
+    color: var(--primary-color);
+  }
 `
-const PatientListwrapper= styled.div`
-width:100%;
-flex:7;
-display:flex;
-flex-direction:column;
-align-items:center;
-overflow:auto;
-max-height:90%;
+const PatientListwrapper = styled.div`
+  width: 100%;
+  flex: 7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: auto;
+  max-height: 90%;
 `
 
-const ChatWrapper= styled.div`
-height:100;
-flex:10;
-display:flex;
-flex-direction:column;
-background-color:var(--background-color);
+const ChatWrapper = styled.div`
+  height: 100;
+  flex: 10;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--background-color);
 `
 const ChatContent = styled.div`
   height: 85%;
@@ -55,140 +57,202 @@ const ChatContent = styled.div`
   padding: 4%;
   overflow: auto;
 
-  .patientName{
-    font-weight:700;
-    font-size:2.3rem;
-    width:85%;
-    margin-bottom:0.5%;
+  .patientName {
+    font-weight: 700;
+    font-size: 2.3rem;
+    width: 85%;
+    margin-bottom: 0.5%;
   }
 
-    &::-webkit-scrollbar{
-        display:none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
-const ChatInput= styled.div`
-height:15%;
-margin-top:2%;
-display:flex;
-justify-content:center;
+const ChatInput = styled.div`
+  height: 15%;
+  margin-top: 2%;
+  display: flex;
+  justify-content: center;
 
-input{
-    width:70%;
-    height:50%;
-    border-radius:10px;
-    border:1px solid var(--primary-color);
-    padding-left:10px;
-    outline:none; 
-}
-button{
-    width:5%;
-    height:50%;
-    background-color:var(--secondary-color);
-    margin-left:3%;
-    border-radius:20%;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-}
+ textarea {
+    width: 70%;
+    height: 50%;
+    border-radius: 10px;
+    border: 1px solid var(--primary-color);
+    padding: 10px;
+    outline: none;
+    overflow: hidden;
+    white-space: pre-wrap; 
+    overflow-wrap: break-word;
+   
+    resize:none
+  } 
+  
 
+  button {
+    width: 5%;
+    height: 50%;
+    background-color: var(--secondary-color);
+    margin-left: 3%;
+    border-radius: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
 const MessageBot = styled.div`
-width:85%;
-height:auto;
-padding:20px;
-background-color:white;
-border:2px solid var(--primary-color);
-border-radius:10px;
+  width: 85%;
+  height: auto;
+  padding: 20px;
+  background-color: white;
+  border: 2px solid var(--primary-color);
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  white-space: pre-wrap; 
+  overflow-wrap: break-word;
+
+  img {
+    margin-right: 20px;
+    height: 30px;
+    width: 50px;
+  }
 `
 const MessageInput = styled.div`
-width:90%;
-height:auto;
-padding:20px;
-background-color:yellow;
+  width: 85%;
+  height: auto;
+  padding: 20px;
+  background-color: #c0e9fc;
+  border-radius: 15px;
+  margin-top: 2%;
+  color: var(--primary-color);
+  white-space: pre-wrap; 
+  overflow-wrap: break-word;
 `
-const PatientArr = [{
-    name:'arshad'
-},
-{
-    name:'yash'
-},
-{
-    name:'yash'
-},
-{
-    name:'yash'
-},
-{
-    name:'yash'
-},
-{
-    name:'yash'
-},
-{
-    name:'yash'
-}]
-
-
+const PatientArr = [
+  {
+    name: 'arshad',
+    age: '21',
+    gender: 'Male'
+  },
+  {
+    name: 'yash',
+    age: '21',
+    gender: 'Male'
+  },
+  {
+    name: 'yash',
+    age: '21',
+    gender: 'Male'
+  },
+  {
+    name: 'yash',
+    age: '21',
+    gender: 'Male'
+  },
+  {
+    name: 'yash',
+    age: '21',
+    gender: 'Male'
+  },
+  {
+    name: 'yash',
+    age: '21',
+    gender: 'Male'
+  }
+]
 
 const ChatApp = () => {
-    const [query , setQuery]=useState('')
-    const[message , setMessage]=useState([{message:'how may i help',role:'bot'}])
-    const[patient , setPatient]=useState('Please Select Patient')
+  const [query, setQuery] = useState('')
+  const [message, setMessage] = useState([
+    { message: 'Please select a patient', role: 'bot' }
+  ])
+  const [patient, setPatient] = useState({
+    name: '',
+    age: '',
+    gender: ''
+  })
 
-    const handleChange = (e) => {
-        setQuery(e.target.value)
+  const handleChange = e => {
+    setQuery(e.target.value)
+  }
+  const handleSend = () => {
+    if (query.trim() !== '') {
+      setMessage([...message, { message: query.trim(), role: 'human' }])
+      setQuery('')
     }
-    const handleSend = () => {
-        if(query.trim() !== ''){
-            setMessage([...message,{message:query.trim() , role:'human'}])
-            setQuery({message:'',role:'human'})
-        }
-    }
-   
+  }
+  useEffect(() => {
+    patient.name !== ''
+      ? setMessage([
+          { message: `Ask me anything about ${patient.name}`, role: 'bot' }
+        ])
+      : setMessage([{ message: `Please select a Patient`, role: 'bot' }])
+  }, [patient.name])
 
-    return (
+  return (
     <Wrapper>
-        <Patients>
-        <div className='title'>Patients</div>
-            <PatientListwrapper>
-                {
-                    PatientArr.map((p)=>(
-                        
-                        <PatientCard name={p.name} onClick={() => setPatient(p.name)}></PatientCard>
-                        
-                       
-                    ))
-                }
-            </PatientListwrapper>
-        </Patients>
+      <Patients>
+        <div className='title'>
+          {' '}
+          <img
+            src={logo}
+            style={{ height: '50px', width: '90px' }}
+            alt='Logo'
+          />
+          Patients
+        </div>
+        <PatientListwrapper>
+          {PatientArr.map(p => (
+            <PatientCard
+              name={p.name}
+              age={p.age}
+              gender={p.gender}
+              onClick={() => {
+                setPatient({
+                  name: p.name,
+                  age: p.age,
+                  gender: p.gender
+                })
+              }}
+            ></PatientCard>
+          ))}
+        </PatientListwrapper>
+      </Patients>
 
-
-        <ChatWrapper>
+      <ChatWrapper>
         <ChatContent>
-        <div className='patientName'>{patient}</div>
-            {
-                message.map((ans)=>(
-                    ans.role === 'bot'?
-                    (<MessageBot>
-                    {ans.message}
-                    </MessageBot>):
-
-                    (<MessageInput>
-                    {ans.message}
-                    </MessageInput>)
-                ))
-            }
+          {patient.age !== '' && (
+            <div
+              className='patientName'
+              style={{ whiteSpace: 'pre' }}
+            >{`${patient.name}     Age: ${patient.age}     Gender: ${patient.gender}`}</div>
+          )}
+          {message.map(ans =>
+            ans.role === 'bot' ? (
+              <MessageBot>
+                <img src={logo}></img>
+                {ans.message}
+              </MessageBot>
+            ) : (
+              <MessageInput>{ans.message}</MessageInput>
+            )
+          )}
         </ChatContent>
 
         <ChatInput>
-        <input value={query.message} onChange={handleChange} placeholder='Enter your query'></input>
-        <button onClick={handleSend}><SendIcon style={{ color: 'white' }}/></button>
-        </ChatInput>
+                {/* <TextareaAutosize value={query} minRows={3} maxRows={3} className='textarea' onChange={handleChange} /> */}
             
-        </ChatWrapper>
-
-     </Wrapper>
+            <textarea onChange={handleChange} value={query}>
+                Enter your query
+            </textarea>
+          <button onClick={handleSend}>
+            <SendIcon style={{ color: 'white' }} />
+          </button>
+        </ChatInput>
+      </ChatWrapper>
+    </Wrapper>
   )
 }
 
--- client/src/components/PatientCard.jsx
+++ client/src/components/PatientCard.jsx
@@ -9,41 +9,51 @@ font-size:1rem;
 margin:2%;
 height: 13%;
 
-border-top:2px solid var(--secondary-color);
+border-top:3px solid var(--secondary-color);
 box-shadow: 0 0 10px hsl(0, 0%, 81%);
 border-radius:0 0 15px 15px;
 padding:2%;
 display:flex;
-span{
+transition: all 0.5s ease;
+&:hover{
 
-   margin-left:4%;
+  transform:scale(1.04);
+}
+div{
+   margin-left:5%;
 }
 .name{
     font-weight:600;
+    font-size:20px;
 }
-.city{
+.age{
     font-weight:400;
-    font-size:12px;
 }
 .info{
     width:100%;
     display:flex;
     flex-direction:column;
-
+}
+.underinfo{
+    margin:0;
+    display:flex;
 }
 `
 
 
-const PatientCard = ({name , onClick}) => {
+const PatientCard = ({name,age,gender , onClick}) => {
   return (
 
    
     <Patient onClick={onClick}>
     <Avatar  sx={{ width: 50, height: 50  }}></Avatar>
     <div className='info'>
-    <span className='name'>{name}</span>
-    <span className='age'>{}</span>
-    <span className='city' >mumbai</span>
+    <div className='name'>{name}</div>
+    <div className='underinfo'>
+    <div className='age'>{age}</div>
+    <div className='gender' >{gender}</div>
+    </div>
+
     </div>
     
     </Patient>
